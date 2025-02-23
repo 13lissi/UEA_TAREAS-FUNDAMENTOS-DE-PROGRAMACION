f991b78b8009c570b9b50c6f7a80b888368242d5
13lissi <la.cornejoo@uea.edu.ec>
2025-02-23 15:02:49 -0500

    Add files via upload

---
+++ Tareas/prueba_13.py
@@ -0,0 +1,13 @@
+#para imprimir los primeros 5 números naturales
+#Iniciamos creando la variable que almacenara nuestros datos
+#comenzamos en uno porque los nuemeros naturales empiezan desde 1
+
+tarea = 1
+
+#Usamos un bucle while para imprimir los primeros 5 números naturales
+#Aplicamos la condicion para que no se nos pase
+while tarea < 6 :
+        print(tarea) #esta linea hara que se muestre en pantalla los numeros empezando por el 1 y su avance
+        tarea += 1  # Aqui usamos un operador de asignacion para incrementar el conteo y cumplir la condicion para finalizar el bucle
+print("cumplido ;) viva Taylor Swift y los gatos ") #crucial!!! no olvidar borrar el espacio de la tabulacion :V
+
