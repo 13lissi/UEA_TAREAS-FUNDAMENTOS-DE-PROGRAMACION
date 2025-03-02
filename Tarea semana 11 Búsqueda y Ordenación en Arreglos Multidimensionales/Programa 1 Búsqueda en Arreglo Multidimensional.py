7c033108ced1a1cc36d90da8010d0bf5fd3b66f9
13lissi <la.cornejoo@uea.edu.ec>
2025-03-02 17:51:38 -0500

    Initial commit

---
+++ Tarea semana 11 Búsqueda y Ordenación en Arreglos Multidimensionales/Programa 1 Búsqueda en Arreglo Multidimensional.py
@@ -0,0 +1,19 @@
+def buscar_valor(matriz, valor):
+    for Fila in range(len(matriz)):
+        for Columna in range(len(matriz[Fila])):
+            if matriz[Fila][Columna] == valor:
+                return print(f" El valor se encontro en la fila- {Fila} columna- {Columna}")  # Retorna la posición (fila, columna)
+    return None  # Retorna None si no se encuentra el valor
+
+# Definimos una matriz 3x3
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# Valor a buscar
+valor_a_buscar = int(input("Introduce el valor a buscar del 1 al 9: "))  # Convertimos a entero
+
+# Llamamos a la función de búsqueda
+resultado = buscar_valor(matriz, valor_a_buscar)
