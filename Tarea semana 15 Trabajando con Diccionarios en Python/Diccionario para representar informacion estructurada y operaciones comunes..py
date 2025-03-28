c3110d5a6a6090e2b139ff4d89abb15d42bc7079
13lissi <la.cornejoo@uea.edu.ec>
2025-03-27 23:42:20 -0500

    Add a new program

--- Tarea semana 15 Trabajando con Diccionarios en Python/Diccionario para representar informacion estructurada y operaciones comunes..py
+++ Tarea semana 15 Trabajando con Diccionarios en Python/Diccionario para representar informacion estructurada y operaciones comunes..py
@@ -17,8 +17,7 @@ if "telefono" not in informacion_personal:
     informacion_personal["telefono"] = "0991510222"  # Si no existe, la agregamos
 
 # Eliminar la clave "edad" del diccionario
-if "edad" in informacion_personal:
-    del informacion_personal["edad"]  # Eliminamos la clave "edad"
+del informacion_personal["edad"]  # Eliminamos la clave "edad"
 
 # Imprimir el diccionario final
 print(informacion_personal)
